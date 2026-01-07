5f399d55ac9a5235c6ed07cfad473ca64f635748
Marian De Silva <mdsw84@gmail.com>
2026-01-07 11:55:46 +0100

    testing api

--- code-alongs/08_fastapi_crud/api.py
+++ code-alongs/08_fastapi_crud/api.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from data_processing import library_data, Book
+
+# list of elements that are pydantic model
+books: list[Book] = library_data("library.json").books
 
 app = FastAPI()
 
 
 @app.get("/books")
 async def read_books():
-    return {"book": "Hello API books"}
+    # can return a pydantic model because
+    # fastapi serializes it to json under the hood
+    return books
+
+
+# path parameter
+@app.get("/book/{id}")
+async def read_book_by_id(id: int):
+    return [book for book in books if book.id == id]
