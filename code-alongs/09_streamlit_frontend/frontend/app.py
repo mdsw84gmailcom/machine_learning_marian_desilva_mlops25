de7e97ac0f7db2bd983db6a18cb931778d7f2214
Marian De Silva <mdsw84@gmail.com>
2026-01-09 13:38:04 +0100

    navigation

--- code-alongs/09_streamlit_frontend/frontend/app.py
+++ code-alongs/09_streamlit_frontend/frontend/app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.markdown("# App front page")
+pages = [
+    st.Page("pages/home.py", title="Home"),
+    st.Page("pages/dashboard.py", title="Dashboard"),
+    st.Page("pages/book_finder.py", title="Bookfinder"),
+]
+
+pg = st.navigation(pages)
+
+pg.run()
